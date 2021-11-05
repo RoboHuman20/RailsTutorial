f190b8a2589bd013d7908841b12375fb7c999ad6
root <root@DESKTOP-EHTOR42.localdomain>
2021-11-05 11:04:31 +0900

    First Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render html:"hello, world."
+        render html:"hello, world!"
     end
-
+    def goodbye
+        render html: "goodbye, world!"
+    end
 end
